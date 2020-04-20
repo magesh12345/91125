999defb6bee8fe6f48a32a2cc361cd2601d531d8
magesh12345 <63988511+magesh12345@users.noreply.github.com>
2020-04-19 22:24:04 -0400

    Update FriendsLevel.java

--- FriendsLevel.java
+++ FriendsLevel.java
@@ -27,3 +27,18 @@ class FriendsLevel {
         System.out.println(countFriendsPairings(n)); 
     } 
 } 
+
+
+/*
+
+Input  : n = 3
+Output : 4
+
+Explanation
+{1}, {2}, {3} : all single
+{1}, {2, 3} : 2 and 3 paired but 1 is single.
+{1, 2}, {3} : 1 and 2 are paired but 3 is single.
+{1, 3}, {2} : 1 and 3 are paired but 2 is single.
+Note that {1, 2} and {2, 1} are considered same.
+
+*/
